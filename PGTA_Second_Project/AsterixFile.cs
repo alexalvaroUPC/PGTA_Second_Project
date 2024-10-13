0d1c3c75e1f27bf60667098833c9001fbc1a967d
aleix <aleix.balust@estudiantat.upc.edu>
2024-10-13 22:54:21 +0200

    - Decode up to SAC and SIC - Decoded messages properly stored in readFile var

--- PGTA_Second_Project/AsterixFile.cs
+++ PGTA_Second_Project/AsterixFile.cs
@@ -8,24 +8,40 @@ namespace PGTA_Second_Project
 {
     public class AsterixFile
     {
-        List<message048> itemList;
+        List<message048> itemList = new List<message048>();
         public List<message048> byteOperations(string fullFile)
         {
+            
             byte[] totalBytes = File.ReadAllBytes(fullFile);
             int i = 0;
             int astLength = totalBytes.Length;
             int k = 0;
             while (i < astLength)
             {
-                /* Propuesta para comprobar que es CAT048
-                 * if totalBytes[i] != 48
-                    DEVOLVER ERROR
-                    break
-                   else
-                    seguir con el bucle
-                */
+                List<byte> FSPEC = new List<byte>();
+                List<byte> messageBytes = new List<byte>();
                 int dataLength = totalBytes[i + 1] + totalBytes[i + 2];
-                this.itemList.Add(new message048(dataItem1, dataItem2, dataItem3));
+                FSPEC.Add(totalBytes[i + 3]);
+                int messageStart = 4;
+                if (totalBytes[i+3] % 2 == 1){
+                    FSPEC.Add(totalBytes[i + 4]);
+                    messageStart++;
+                    if (totalBytes[i + 4] % 2 == 1)
+                    {
+                        FSPEC.Add(totalBytes[i + 5]);
+                        messageStart++;
+                        if (totalBytes[i + 5] % 2 == 1)
+                        {
+                            FSPEC.Add(totalBytes[i + 6]);
+                            messageStart++;
+                        }
+                    }
+                }
+                for(int j = i+messageStart; j<i+dataLength; j++)
+                {
+                    messageBytes.Add(totalBytes[j]);
+                }
+                this.itemList.Add(new message048(dataLength, FSPEC, messageBytes));
                 i = i + dataLength;
                 k = k + 1;
             }
--- PGTA_Second_Project/message048.cs
+++ PGTA_Second_Project/message048.cs
@@ -3,22 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 
 namespace PGTA_Second_Project
 {
     public class message048
     {
         private int length;
-        private int ID;
-        private int dataItem1;
-        private int dataItem2;
-        private int dataItem3;
+        private List<byte> message;
+        private string SAC;
+        private string SIC;
 
-        public message048(int dataItem1, int dataItem2, int dataItem3)
+        public message048(int length, List<byte> FSPEC, List<byte> fullMessage)
         {
-            this.dataItem1 = dataItem1;
-            this.dataItem2 = dataItem2;
-            this.dataItem3 = dataItem3;
+            this.length = length;
+            this.message = fullMessage;
+            int[] arrayFSPEC1 = dec2bin(FSPEC[0], 8);
+            int byteCount = 0;
+            if (arrayFSPEC1[0] == 1)
+            {
+                this.SAC = Convert.ToString(fullMessage[0]);
+                this.SIC = Convert.ToString(fullMessage[1]);
+                byteCount = byteCount + 2;
+            }
+
+            return;
+        }
+        public int[] dec2bin(int decimalNum, int bitNumber)
+        {
+            int[] bitArray = new int[bitNumber];
+            int[] auxArray = new int[bitNumber];
+            int i = 0;
+            while (decimalNum > 0)
+            {
+                auxArray[i] = decimalNum % 2;
+                decimalNum /= 2;
+                i++;
+            }
+            for (int j = 0; j < bitNumber; j++)
+            {
+                bitArray[(bitNumber - j-1)] = auxArray[j];
+            }
+            return bitArray;
         }
     }
 }
