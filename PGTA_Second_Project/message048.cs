db89790f935e1c735f7a81749939e017160400d3
aleix <aleix.balust@estudiantat.upc.edu>
2024-10-14 19:53:10 +0200

    Time of the day computations

--- PGTA_Second_Project/message048.cs
+++ PGTA_Second_Project/message048.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms.VisualStyles;
-
 namespace PGTA_Second_Project
 {
     public class message048
@@ -13,6 +12,7 @@ namespace PGTA_Second_Project
         private List<byte> message;
         private string SAC;
         private string SIC;
+        private string timeOfDay;
 
         public message048(int length, List<byte> FSPEC, List<byte> fullMessage)
         {
@@ -22,11 +22,39 @@ namespace PGTA_Second_Project
             int byteCount = 0;
             if (arrayFSPEC1[0] == 1)
             {
-                this.SAC = Convert.ToString(fullMessage[0]);
-                this.SIC = Convert.ToString(fullMessage[1]);
+                this.SAC = Convert.ToString(fullMessage[byteCount]);
+                this.SIC = Convert.ToString(fullMessage[byteCount+1]);
                 byteCount = byteCount + 2;
             }
+            if (arrayFSPEC1[1] == 1)
+            {
+                this.timeOfDay = timeFinder(fullMessage[byteCount], fullMessage[byteCount+1], fullMessage[byteCount+2]);
+                byteCount = byteCount + 3;
+            }
+            if (arrayFSPEC1[2] == 1)
+            {
 
+            }
+            if (arrayFSPEC1[3] == 1)
+            {
+                
+            }
+            if (arrayFSPEC1[4] == 1)
+            {
+
+            }
+            if (arrayFSPEC1[5] == 1)
+            {
+
+            }
+            if (arrayFSPEC1[6] == 1)
+            {
+
+            }
+            if (arrayFSPEC1[7] == 1)
+            {
+                int[] arrayFSPEC2 = dec2bin(FSPEC[1], 8);
+            }
             return;
         }
         public int[] dec2bin(int decimalNum, int bitNumber)
@@ -45,6 +73,38 @@ namespace PGTA_Second_Project
                 bitArray[(bitNumber - j-1)] = auxArray[j];
             }
             return bitArray;
+        }
+        public string timeFinder(int octet1, int octet2, int octet3)
+        {
+            int[] bits1 = dec2bin(octet1, 8);
+            int[] bits2 = dec2bin(octet2, 8);
+            int[] bits3 = dec2bin(octet3, 8);
+            int[] fullbits = new int[24];
+            double foundTime = 0;
+            for (int i = 0; i < bits1.Length; i++)
+            {
+                fullbits[i] = bits1[i];
+            }
+            for (int i = bits1.Length; i < bits1.Length+bits2.Length; i++)
+            {
+                fullbits[i] = bits2[i-bits1.Length];
+            }
+            for (int i = bits1.Length + bits2.Length; i < bits1.Length + bits2.Length + bits3.Length; i++)
+            {
+                fullbits[i] = bits3[i - (bits1.Length + bits2.Length)];
+            }
+            for (int i = 0; i < 24; i++)
+            {
+                if (fullbits[i] != 0)
+                {
+                    foundTime = foundTime + Math.Pow(2, 23-i) * 1 / 128;
+                }
+            }
+            int hours = (int)Math.Floor(foundTime / 3600);
+            int minutes = (int)Math.Floor(foundTime/60 - 60*hours);
+            double seconds = foundTime-3600*hours-60*minutes;
+            string timeHHMMSS = $"{ hours } : {minutes} : {seconds}";
+            return timeHHMMSS;
         }
     }
 }
