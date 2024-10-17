152ab3f1f5072da893420fcca21cb7a3c95295dc
aleix <aleix.balust@estudiantat.upc.edu>
2024-10-17 19:07:14 +0200

    Added binary to decimal conversion Mode3A octal representation

--- PGTA_Second_Project/message048.cs
+++ PGTA_Second_Project/message048.cs
@@ -13,6 +13,10 @@ namespace PGTA_Second_Project
         private string SAC;
         private string SIC;
         private string timeOfDay;
+        private string mode3V;
+        private string mode3G;
+        private string mode3L;
+        private string mode3squawk;
 
         public message048(int length, List<byte> FSPEC, List<byte> fullMessage)
         {
@@ -41,7 +45,8 @@ namespace PGTA_Second_Project
             }
             if (arrayFSPEC1[4] == 1)
             {
-
+                this.mode3squawk = mode3AOctal(fullMessage[byteCount], fullMessage[byteCount + 1]);
+                byteCount = byteCount + 2;
             }
             if (arrayFSPEC1[5] == 1)
             {
@@ -74,6 +79,19 @@ namespace PGTA_Second_Project
             }
             return bitArray;
         }
+        public double bin2dec(int[] bits) {
+            int count = bits.Length;
+            double decimalNum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (bits[i] != 0)
+                {
+                    decimalNum = decimalNum + Math.Pow(2,count-1-i);
+                }
+            }
+            return decimalNum;
+
+        }
 
         public int hex2dec(string hexValue)
         {
@@ -84,18 +102,39 @@ namespace PGTA_Second_Project
             double foundTime = 0;
             int fullnumber = (octet1 << 16) | (octet2 << 8) | octet3;
             int[] fullbits = dec2bin(fullnumber, 24);
-            for (int i = 0; i < 24; i++)
-            {
-                if (fullbits[i] != 0)
-                {
-                    foundTime = foundTime + Math.Pow(2, 23-i) * 1 / 128;
-                }
-            }
+            foundTime = bin2dec(fullbits) / 128;
             int hours = (int)Math.Floor(foundTime / 3600);
             int minutes = (int)Math.Floor(foundTime/60 - 60*hours);
             double seconds = foundTime-3600*hours-60*minutes;
             string timeHHMMSS = $"{ hours } : {minutes} : {seconds}";
             return timeHHMMSS;
         }
+       public string mode3AOctal(int octet1, int octet2)
+        {
+            int fullnumber = (octet1<<8) | octet2;
+            int[] fullbits = dec2bin(fullnumber, 16);
+            if (fullbits[0] == 1) {
+                this.mode3V = "Validated";
+            }
+            if (fullbits[1] == 1)
+            {
+                this.mode3G = "Garbled";
+            }
+            if (fullbits[2] == 1)
+            {
+                this.mode3L = "Not extracted during last scan";
+            }
+            string squawk = null;
+            for (int i = 4; i < 16; i+=3) {
+                int[] squawkBits = new int[3];
+                squawkBits[0] = fullbits[i];
+                squawkBits[1] = fullbits[i+1];
+                squawkBits[2] = fullbits[i+2];
+                squawk = squawk + bin2dec(squawkBits);
+            }
+            return squawk;
+
+        }
+       
     }
 }
